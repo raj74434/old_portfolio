41c8e3bb5ea0e99ae23b19bdfe563dc443ae19dd
raj74434 <rajgupta74434@gmail.com>
2022-10-16 16:08:17 +0530

    nav bar

---
+++ script/index.js
@@ -0,0 +1,35 @@
+
+
+  
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Java Backend Developer"],
+        typeSpeed: 100,
+        backSpeed: 120,
+        loop: true,
+      });
+    
+      var typed = new Typed(".typing-2", {
+        strings: ["Java Backend Developer"],
+        typeSpeed: 100,
+        backSpeed: 120,
+        loop: true,
+      });
+    
+      const myCarouselElement = document.querySelector("#carouselExampleFade");
+  const carousel = new bootstrap.Carousel(myCarouselElement, {
+      interval: 2500,
+  });
+  
+  
+  let sidemeu = document.querySelector("#sidemenu");
+  
+  function openmenu() {
+      sidemeu.style.right = "0";
+  }
+  
+  function closemenu() {
+      sidemeu.style.right = "-20px";
+  }
+  
+  
